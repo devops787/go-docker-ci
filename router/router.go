c9cb315371bef03fbdcc820bd039a7627ddd329e
devops787 <devops787@gmail.com>
2019-07-15 10:57:39 +0300

    Refactored route handlers

--- router/router.go
+++ router/router.go
@@ -18,123 +18,128 @@ func NewRouter() *mux.Router {
 		writer.Write([]byte("Hello world!"))
 	})
 
-	// add
-	router.HandleFunc("/add/{x:[0-9]+}/{y:[0-9]+}", func(writer http.ResponseWriter, request *http.Request) {
-		vars := mux.Vars(request)
-
-		x, err := strconv.ParseInt(vars["x"], 10, 32)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		y, err := strconv.ParseInt(vars["y"], 10, 32)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		result := calc.Add(int32(x), int32(y))
-		response := map[string]interface{} {
-			"result": result,
-		}
-
-		writer.WriteHeader(http.StatusOK)
-		writer.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(writer).Encode(response)
-	}).Methods("GET")
-
-	// subtract
-	router.HandleFunc("/sub/{x:[0-9]+}/{y:[0-9]+}", func(writer http.ResponseWriter, request *http.Request) {
-		vars := mux.Vars(request)
-
-		x, err := strconv.ParseInt(vars["x"], 10, 32)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		y, err := strconv.ParseInt(vars["y"], 10, 32)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		result := calc.Subtract(int32(x), int32(y))
-		response := map[string]interface{} {
-			"result": result,
-		}
-
-		writer.WriteHeader(http.StatusOK)
-		writer.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(writer).Encode(response)
-	}).Methods("GET")
-
-	// multiply
-	router.HandleFunc("/mul/{x:[0-9]+}/{y:[0-9]+}", func(writer http.ResponseWriter, request *http.Request) {
-		vars := mux.Vars(request)
-
-		x, err := strconv.ParseInt(vars["x"], 10, 32)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		y, err := strconv.ParseInt(vars["y"], 10, 32)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		result := calc.Multiply(int32(x), int32(y))
-		response := map[string]interface{} {
-			"result": result,
-		}
-
-		writer.WriteHeader(http.StatusOK)
-		writer.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(writer).Encode(response)
-	}).Methods("GET")
-
-	// divide
-	router.HandleFunc("/div/{x:[0-9]+}/{y:[0-9]+}", func(writer http.ResponseWriter, request *http.Request) {
-		vars := mux.Vars(request)
-
-		x, err := strconv.ParseInt(vars["x"], 10, 32)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		y, err := strconv.ParseInt(vars["y"], 10, 32)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		result, err := calc.Divide(int32(x), int32(y))
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		response := map[string]interface{} {
-			"result": result,
-		}
-
-		writer.WriteHeader(http.StatusOK)
-		writer.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(writer).Encode(response)
-	}).Methods("GET")
+	router.HandleFunc("/add/{x:[0-9]+}/{y:[0-9]+}", AddHandler).Methods("GET")
+	router.HandleFunc("/sub/{x:[0-9]+}/{y:[0-9]+}", SubtractHandler).Methods("GET")
+	router.HandleFunc("/mul/{x:[0-9]+}/{y:[0-9]+}", MultiplyHandler).Methods("GET")
+	router.HandleFunc("/div/{x:[0-9]+}/{y:[0-9]+}", DivideHandler).Methods("GET")
 
 	return router
+}
+
+// Add route handler
+func AddHandler(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+
+	x, err := strconv.ParseInt(vars["x"], 10, 32)
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	y, err := strconv.ParseInt(vars["y"], 10, 32)
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	result := calc.Add(int32(x), int32(y))
+	response := map[string]interface{} {
+		"result": result,
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(response)
+}
+
+// Subtract route handler
+func SubtractHandler(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+
+	x, err := strconv.ParseInt(vars["x"], 10, 32)
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	y, err := strconv.ParseInt(vars["y"], 10, 32)
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	result := calc.Subtract(int32(x), int32(y))
+	response := map[string]interface{} {
+		"result": result,
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(response)
+}
+
+// Multiply route handler
+func MultiplyHandler(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+
+	x, err := strconv.ParseInt(vars["x"], 10, 32)
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	y, err := strconv.ParseInt(vars["y"], 10, 32)
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	result := calc.Multiply(int32(x), int32(y))
+	response := map[string]interface{} {
+		"result": result,
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(response)
+}
+
+// Divide route handler
+func DivideHandler(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+
+	x, err := strconv.ParseInt(vars["x"], 10, 32)
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	y, err := strconv.ParseInt(vars["y"], 10, 32)
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	result, err := calc.Divide(int32(x), int32(y))
+	if err != nil {
+		log.Println(err)
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	response := map[string]interface{} {
+		"result": result,
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(response)
 }
